f066812e40d6e824f06abd16cc7c7340c731da01
Ethanzjp <crimezjp@163.com>
2022-07-12 08:32:05 +0800

    modify virtual function table pointer layout code

--- mm3_virtual_function_layout.cpp
+++ mm3_virtual_function_layout.cpp
@@ -31,61 +31,62 @@ public:
 };
 
 int main(){
-	// Base1 b1;
+	Base1 b1;
 
 
-	// Sub s;
+	Sub s;
 
-	// b1=s;
+	b1=s;
 
-	// b1.d1=100;
-	// Base2 b2;
-	// b2.d2=200;
-	// Sub s;
-	// s.d1=100;
-	// s.d2=200;
-	// s.d3=300;
-	// cout<<sizeof(b1)<<endl;
-	// cout<<sizeof(b2)<<endl;
+	b1.d1=100;
+	Base2 b2;
+	b2.d2=200;
+	Sub s;
+	s.d1=100;
+	s.d2=200;
+	s.d3=300;
+	cout<<sizeof(b1)<<endl;
+	cout<<sizeof(b2)<<endl;
 	cout<<sizeof(Sub)<<endl; //16+16+8 = 32+8=40
 
-	// long long* p1=(long long*)&b1;
-	// long long* pvt1=p1;
-	// cout<<*p1<<",";
-	// p1++;
-	// cout<<*p1<<endl;
+	long long* p1=(long long*)&b1;
+	long long* pvt1=p1;
+	cout<<*p1<<",";
+	p1++;
+	cout<<*p1<<endl;
 
-    // long long* p2=(long long*)&b2;
-    // long long* pvt2=p2;
-    // cout<<*p2<<",";
-    // p2++;
-    // cout<<*p2<<endl;
+    long long* p2=(long long*)&b2;
+    long long* pvt2=p2;
+    cout<<*p2<<",";
+    p2++;
+    cout<<*p2<<endl;
 
-    // long long* p3=(long long*)&s;
-    // long long* pvt3=p3;
-    // cout<<*p3<<",";
-    // p3++;
-    // cout<<*p3<<",";
-    // p3++;
-    // long long* pvt4=p3;
-    // cout<<*p3<<",";
-    // p3++;
-    // cout<<*p3<<",";
-    // p3++;
-    // cout<<*p3<<endl;
+    long long* p3=(long long*)&s;
+    long long* pvt3=p3;
+    cout<<*p3<<",";
+    p3++;
+    cout<<*p3<<",";
+    p3++;
+    long long* pvt4=p3;
+    cout<<*p3<<",";
+    p3++;
+    cout<<*p3<<",";
+    p3++;
+    cout<<*p3<<endl;
 
 
-    // cout<<*pvt1<<":"<< *((long long*)(*pvt1))<<endl;
-    // cout<<*pvt2<<":"<< *((long long*)(*pvt2))<<endl;
+    cout<<*pvt1<<":"<< *((long long*)(*pvt1))<<endl;
+    cout<<*pvt2<<":"<< *((long long*)(*pvt2))<<endl;
     
-    // cout<<*pvt3<<":"<< *((long long*)(*pvt3))<<endl;
+    cout<<*pvt3<<":"<< *((long long*)(*pvt3))<<endl;
 
-    // cout<<*pvt4<<":"<< *((long long*)(*pvt4))<<endl;
+    cout<<*pvt4<<":"<< *((long long*)(*pvt4))<<endl;
 }
 
 
 
 /*
+cout<<sizeof(Sub)<<endl; //16+16+8 = 32+8=40 的结果如下：
 reference: https://cloud.tencent.com/developer/article/1755489
 *** Dumping AST Record Layout                                                   
   27          0 | class Base1                                                        
